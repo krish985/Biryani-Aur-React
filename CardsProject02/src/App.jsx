c52dae3038dbd62bf59bca4750388980936a1212
krish985 <krishkumar9810@gmail.com>
2024-07-21 07:52:00 +0530

    create cards projrct

---
+++ CardsProject02/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Cards from "./component/cards"
+function App() {
+
+
+  return (
+    <>
+      
+    <Cards username="Alex" txtBtn="visit"/><br />
+    <Cards username="John" txtBtn="veiw more"/>
+   
+       
+      
+    </>
+  )
+}
+
+export default App
